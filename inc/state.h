622f9421ccdca81c8d6c9dce18cb303eb28d34b3
Borroot <borroot@live.nl>
2020-04-14 22:11:54 +0200

    Add documentation of the state struct.

--- inc/state.h
+++ inc/state.h
@@ -1,3 +1,10 @@
+/**
+ * @file
+ *
+ * @brief This file defines the structs for a card,
+ * player and a game state.
+ */
+
 #ifndef STATE_H
 # define STATE_H
 
@@ -5,18 +12,80 @@
 
 using namespace std;
 
+/**
+ * @brief The information connected to a card.
+ *
+ * @note We use a struct instead of a typedef in case
+ * it turns out that we need more information later on
+ * in the development process.
+ */
 struct Card {
-	vector<bool> players;  // The players which could have this card.
+
+	/**
+	 * @brief The players which could have this card.
+	 *
+	 * This is denoted by true if the player can have
+	 * the card and false otherwise. To give an example,
+	 * [0,1,0] means that player 0 and 2 cannot have
+	 * this card, but player 1 can have this card.
+	 */
+	vector<bool> players;
 };
 
+/**
+ * @brief The information connected to a player.
+ */
 struct Player {
-	int num_cards;         // The number of cards the player has.
-	vector<int> sets;      // The number of cards the player has from a certain set.
+
+	/**
+	 * @brief The number of cards this player has.
+	 */
+	int num_cards;
+
+	/**
+	 * @brief The number of cards the player at least
+	 * has from a certain set/category.
+	 *
+	 * To give an example, [0,3,1] means that this player
+	 * has at least 0 cards from set/category 0, it has
+	 * at least 3 cards from set/category 1 and it has at
+	 * least 1 card from set/category 2.
+	 */
+	vector<int> sets;
 };
 
+/**
+ * @brief The complete game state.
+ *
+ * This struct contains all the information of the game
+ * state at a specific moment in time.
+ */
 struct State {
+
+	/**
+	 * @brief The player who is on turn.
+	 */
 	int onturn;
+
+	/**
+	 * @brief The information on all the cards.
+	 *
+	 * This vector also contains information on which card
+	 * is in what category and which card they are with
+	 * respect to their category. Consider the following
+	 * example [card0, ..., card7] and we have a SET_SIZE
+	 * of 4 and thus a NUM_SETS of 2 then the cards [card0, ..., card3]
+	 * belong to set/category 0 and [card4, ..., card7] belong to
+	 * set/category 1. In these examples card4 will be card 0 with
+	 * respect to its own set/category and card7 will be card 3.
+	 */
 	vector<Card> cards;
+
+	/**
+	 * @brief The information on all the players.
+	 *
+	 * This is where it is defined who is player 0, 1, etc.
+	 */
 	vector<Player> players;
 };
 
